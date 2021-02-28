d7af0475723af207ad6a77b8fb30a2ed3ec70548
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-02-28 10:40:36 +0100

    Create Car.cs

---
+++ Car/Car.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klasa1
+{
+    public class Car
+    {
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int NumberofDoors { get; set; }
+    }
+}
