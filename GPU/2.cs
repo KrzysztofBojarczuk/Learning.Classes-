d775c0b2412bcfdb86b08e068ac6eccb01691068
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-02 17:13:16 +0100

    Create 2.cs

---
+++ GPU/2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp20
+{
+    class Gpu
+    {
+        public string Name { get; set; } = "RTX3070";
+        public string Color { get; set; } = "Green";
+        public decimal price { get; set; } = 5000m;
+        public int RAM { get; set; } = 16;
+
+        public bool dostepnosc { get; set; } = false;
+    }
+}
