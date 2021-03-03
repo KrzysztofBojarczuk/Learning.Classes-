241cce167ffa988503c4d8029c0487dd4e7d1990
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-03 17:28:07 +0100

    Create 2.cs

---
+++ Samolot/2.cs
@@ -0,0 +1,10 @@
+namespace Samolot
+{
+    class Samolot
+    {
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int NumbersOfSeats { get; set; }
+
+    }
+}
