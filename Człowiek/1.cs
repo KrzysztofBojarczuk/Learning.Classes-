60a423cf9e617df364b15215f35d580d424996ae
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-04 18:53:51 +0100

    Create 1.cs

---
+++ Człowiek/1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klasa2
+{
+    class Człowiek
+    {
+        public string Color { get; set; } = "Biały";
+        public int Year { get; set; } = 1988;
+        public int NumberOfLegs { get; set; } = 2;
+    }
+}
