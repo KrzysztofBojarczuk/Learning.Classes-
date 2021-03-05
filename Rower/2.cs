8a3c4b9371f743d6bf2e9839eba023d518a1317e
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-05 10:41:28 +0100

    Create 2.cs

---
+++ Rower/2.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klasa3
+{
+    class Rower
+    {
+        public int ilośćkół { get; set; }
+        public int siedzenie { get; set; }
+        public string rodzaj { get; set; }
+    }
+}
