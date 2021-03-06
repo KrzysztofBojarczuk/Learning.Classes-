dcf25429cba0bcb6f8ea4b617eb2b0c794131a74
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-06 14:33:03 +0100

    Create 1.cs

---
+++ Resturacja/1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klasa4
+{
+    class Resturacja
+    {
+        public int Menu { get; set; }
+        public string Miasto { get; set; }
+        public bool DlaUbogich { get; set; }
+    }
+}
