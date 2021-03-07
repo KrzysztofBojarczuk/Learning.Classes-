0c9bd196419551ac8bdc7272358c3bcb297b9246
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-07 17:14:02 +0100

    Create 1.cs

---
+++ Intel/1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klasa5
+{
+    public class Intel
+    {
+        public string Model { get; set; } = "i7";
+        public decimal Cenna { get; set; } = 1500m;
+        public int Rdzenie { get; set; } = 8;
+        public bool Dostępność { get; set; } = true;
+    }
+}
