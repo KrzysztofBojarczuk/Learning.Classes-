ab6d6b16154d90011fdcd0f90af51ed6fb1a715a
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-09 16:12:48 +0100

    Create 1.cs

---
+++ File/1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace klasa7
+{
+    public class File
+    {
+        public string FileName { get; set; }
+        public int Size { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
