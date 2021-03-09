29fe452abbe47ef3cc369d30d5d96badd1f8527c
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-09 16:13:07 +0100

    Create Excel.cs

---
+++ File/Excel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace klasa7
+{
+    public class Excel : File
+    {
+        
+
+        public string Niemcy { get; set; }
+
+        public void DoOdczytu()
+        {
+            Console.WriteLine("Plik do odczytu");
+        }
+    }
+}
