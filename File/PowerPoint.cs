cb5fdda962422de9fa4fa99535441d838210bffc
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-09 16:13:41 +0100

    Create PowerPoint.cs

---
+++ File/PowerPoint.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace klasa7
+{
+    class PowerPoint : File
+    {
+        
+        public string Francja { get; set; }
+    }
+}
