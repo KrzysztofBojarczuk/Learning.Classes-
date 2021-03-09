aebc482f7442beaf1504adcd7798a7247280035a
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-09 16:13:25 +0100

    Create Word.cs

---
+++ File/Word.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace klasa7
+{
+    class Word : File
+    {
+        public string USA { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Plik do wydruku");
+        }
+    }
+}
