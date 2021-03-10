e8c9fcdc50bb9dd5bf8e5997f1378e827e351505
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-10 16:56:13 +0100

    Create Heatchback.cs

---
+++ DziedziczenieCars/Heatchback.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2
+{
+    class Heatchback : Car
+    {
+       
+        public DateTime DateTime { get; set; }
+
+    }
+}
