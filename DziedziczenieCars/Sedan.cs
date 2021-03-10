05ced1bfb9be0b98b4d16d9dc5bf75c5ed56a66c
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-10 16:56:42 +0100

    Create Sedan.cs

---
+++ DziedziczenieCars/Sedan.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2
+{
+    class Sedan : Car
+    {
+       
+        public DateTime DateTime { get; set; }
+    }
+}
