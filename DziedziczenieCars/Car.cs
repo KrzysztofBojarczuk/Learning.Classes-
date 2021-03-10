f2d56080d4c0b810b5cca91d3fca2be114be3669
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-10 16:55:57 +0100

    Create Car.cs

---
+++ DziedziczenieCars/Car.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2
+{
+    class Car
+    {
+        public int NumberOfdoors { get; set; }
+        public string Engine { get; set; }
+        public int NumberOfSeats { get; set; }
+    }
+}
