d7631a134108122d44aecf4ba813110fb348c68c
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-10 16:56:27 +0100

    Create Suv.cs

---
+++ DziedziczenieCars/Suv.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2
+{
+    class Suv : Car
+    {
+       
+        public DateTime DateTime { get; set; }
+    }
+}
