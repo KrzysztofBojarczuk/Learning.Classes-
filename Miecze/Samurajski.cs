35e7be677cbe4c6dd383459fe209ab4a2df173c1
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-12 19:34:45 +0100

    Create Samurajski.cs

---
+++ Miecze/Samurajski.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8
+{
+    public class Samurajski : Miecz
+    {
+        public string Kraj { get; set; } = "Japonii";
+        public string Prosty { get; set; } = "Prosty";
+        public string Rodzaj { get; set; } = "Oburączny";
+    }
+}
