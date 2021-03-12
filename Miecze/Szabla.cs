7600a3c51079920c9d726b81f9b94a19de65c618
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2021-03-12 19:42:49 +0100

    Create Szabla.cs

---
+++ Miecze/Szabla.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8
+{
+    class Szabla : Miecz
+    {
+        public string Kraj { get; set; } = "Polskaa";
+        public string Prosty { get; set; } = "Krzywa";
+        public string Rodzaj { get; set; } = "Oburączny";
+    }
+}
